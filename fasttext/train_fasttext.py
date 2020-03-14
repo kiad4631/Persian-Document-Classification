0867c6490f9d62baebeb7544cc952bd817e5f785
Kia <46137886+Davari393@users.noreply.github.com>
2020-03-14 01:24:04 -0400

    Update train_fasttext.py

--- fasttext/train_fasttext.py
+++ fasttext/train_fasttext.py
@@ -45,6 +45,7 @@ def read_csv(filename):
     return X, Y
 
 # Split train and test data with ratio 0.8, 0.2 respectively
+# Data already shuffled by excel
 data_xlsx.iloc[0:int(len(data_xlsx)*0.8)].to_csv('train.csv' , encoding = "utf-8" , index = False)
 data_xlsx.iloc[int(len(data_xlsx)*0.8):].to_csv('test.csv' , encoding = "utf-8" , index = False)
 
